ea9930386256c45c89c27d1ffb198fa71cd97b6b
DP-0P <deepakpattnayak2013@gmail.com>
2021-11-06 22:24:06 +0530

    dj is online...

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+const config = require("./Data/config.json");
+
+const intents = new Discord.Intents(32767);
+
+const client = new Discord.Client({intents});
+
+client.on("ready",() => console.log("Bot is online..."));
+
+client.on("messageCreate",message =>{
+    console.log(message.content);
+
+    if(message.content == "hello") message.reply("sup?");
+});
+
+client.login(config.token);

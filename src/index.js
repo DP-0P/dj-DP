75f2ee98a582aadb16abbab9bc2647608540aa45
DP-0P <deepakpattnayak2013@gmail.com>
2021-11-07 00:11:00 +0530

    commands added...

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-const Discord = require("discord.js");
+const Client = require("./structures/client.js");
+
+const Command = require("./structures/command.js");
 
 const config = require("./Data/config.json");
 
-const intents = new Discord.Intents(32767);
+const client = new Client();
 
-const client = new Discord.Client({intents});
+const fs = require("fs");
 
-client.on("ready",() => console.log("Bot is online..."));
+fs.readdirSync("./src/commands")
+    .filter(file => file.endsWith(".js")).
+    forEach(file => {
+    /**
+     * @type {Command}
+     */
+    const command = require(`./commands/${file}`);
+    console.log(`Command ${command.name} loaded...`);
+    client.commands.set(command.name,command);
+
+});
+
+client.on("ready",() => {
+    console.log("Bot is online...");
+    client.user.setActivity("with ur mom!");
+});
 
 client.on("messageCreate",message =>{
     console.log(message.content);
 
-    if(message.content == "hello") message.reply("sup??");
+    // if(message.content == "hello") message.reply("sup??");
+
+    if(!message.content.startsWith(config.prefix)) return;
+
+    const args = message.content.substring(config.prefix.length).split(/ +/);
+
+    const command = client.commands.find(cmd => cmd.name == args[0]);
+
+    if(!command) return message.reply (`${args[0]} is not a valid command...`);
+
+    command.run(message,args,client);
+
 });
 
 client.login(config.token);

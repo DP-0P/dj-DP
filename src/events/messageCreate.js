166ed9c3a6bc7028d8dd1ca0dc5367254fc98ef3
DP-0P <deepakpattnayak2013@gmail.com>
2021-11-08 20:02:06 +0530

    reply changed

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -9,7 +9,7 @@ module.exports = new Event("messageCreate", (client, message) => {
 
 	const command = client.commands.find(cmd => cmd.name == args[0]);
 
-	if (!command) return message.reply(`${args[0]} is not a valid command!`);
+	if (!command) return message.reply(`**${args[0]}** is not a valid command **U DumbFck** !`);
 
 	command.run(message, args, client);
 });

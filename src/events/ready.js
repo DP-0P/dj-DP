f9ec9d2359dc297fd93b341a95a9c658223c3cc5
DP-0P <deepakpattnayak2013@gmail.com>
2021-11-09 16:08:51 +0530

    play function added

--- src/events/ready.js
+++
@@ -1,6 +0,0 @@
-const Event = require("../structures/event.js");
-
-module.exports = new Event("ready", client => {
-	console.log("Bot is ready!");
-    client.user.setActivity("with ur mom!");
-});
--- src/index.js
+++
@@ -1,7 +0,0 @@
-const Client = require("./structures/client.js");
-
-const config = require("./data/config.json");
-
-const client = new Client();
-
-client.start(config.token);

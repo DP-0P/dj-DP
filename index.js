0243f766fde07824c0dde198e831556385b094d4
DP-0P <deepakpattnayak2013@gmail.com>
2021-11-10 16:36:55 +0530

    all commands added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const fs = require("fs");
 const Discord = require("discord.js");
-const Client = require("./client/Client");
+// const Client = require("./client/Client");
 const config = require("./config.json");
 const { Player } = require("discord-player");
 const { MessageEmbed } = require('discord.js');
